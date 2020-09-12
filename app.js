de71d60401fb0250a9f2fc0364715707ed44c37a
Rishav Gooptu <rishavgooptu1234@gmail.com>
2020-09-13 01:01:51 +0530

    3

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-mongoose.connect('mongodb+srv://Rishav:spNKHfxqUunHlunx@cluster0.192oi.mongodb.net/Users?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://Rishav:spNKHfxqUunHlunx@cluster0.192oi.mongodb.net/Users1?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
 const db = mongoose.connection;
 db.on('error',()=>{
     console.log(error);
